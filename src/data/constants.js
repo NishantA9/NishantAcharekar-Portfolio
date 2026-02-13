15d40d2cf6451efa8c8c70347e415196b0a2d974
NishantA9 <nishantacharekar12@gmail.com>
2026-02-12 18:10:38 -0600

    updated resume link sde

--- src/data/constants.js
+++ src/data/constants.js
@@ -19,7 +19,7 @@ export const Bio = {
   github: "https://github.com/NishantA9",
   // resume:
   //   "https://drive.google.com/file/d/1lOXuliPrdNwPrOHU3f3sISXHziyBM36-/view?usp=sharing",
-  resumeSDE: "https://drive.google.com/file/d/17PiTGOPIf8T4ThWGx8U0kHaL17Zk9y1y/view?usp=sharing",
+  resumeSDE: "https://drive.google.com/file/d/1QSdZ_-Q_E5IJnfzwXj24904TvqTdDlHA/view?usp=sharing",
   resumeData: "https://drive.google.com/file/d/1awY3fDEWlHi-7LdavQkn5yPlArrqSzoi/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/nishantacharekar/",
   twitter: "https://x.com/fireemperor9",
